70a20b098bcc5b820626e3b5380c5de89e912f40
Victor323232 <victorhugoabc59@gmail.com>
2022-04-17 15:44:10 -0300

    Metodo gerar frase

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main(){
   runApp(MaterialApp(
@@ -16,6 +17,24 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  var _frases = [
+    "Sem dor, sem ganho. Cale a boca e treine.",
+    "Seu corpo pode suportar quase tudo. É a sua mente que você tem que convencer.",
+    "O sucesso começa com autodisciplina.",
+    "Coisas boas vêm para aqueles que suam.",
+    "Um treino de uma hora é 4% do seu dia. Sem desculpas.",
+    "Apresse esse músculo.",
+    "Trabalhe duro em silêncio. Deixe o sucesso ser o seu barulho."
+
+  ];
+  var _fraseGerada = "Clique abaixo para gerar uma frase";
+  void _gerarFrase(){
+    var numeroSorteado = Random().nextInt( _frases.length);
+    setState(() {
+      _fraseGerada = _frases[numeroSorteado];
+    });
+
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,14 +44,14 @@ class _HomeState extends State<Home> {
       ),
       body: Center(
         child: Container(
-            padding: EdgeInsets.only(top:10),
+            padding: EdgeInsets.all(10),
             child: Column(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 crossAxisAlignment:CrossAxisAlignment.center,
                 children:<Widget>[
                   Image.asset("images/logo1.png"),
                   Text(
-                      "Clique abaixo para gerar uma frase",
+                    _fraseGerada,
                       textAlign:TextAlign.justify,
                       style: TextStyle(
                           fontSize: 20,
@@ -53,7 +72,7 @@ class _HomeState extends State<Home> {
                     style: TextButton.styleFrom(
                         backgroundColor: Colors.black
                     ),
-                    onPressed: (){},
+                    onPressed: _gerarFrase,
                   )
                 ]
             )
